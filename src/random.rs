0264718f82b678a3988dee18184195acea70560a
Sam Gebhardt <samgeb35@gmail.com>
2020-12-17 13:37:44 -0800

    Random correctly selects a piece

--- src/random.rs
+++ src/random.rs
@@ -5,35 +5,40 @@ use crate::pieces::Moves;
 use crate::pieces::piece_type;
 use crate::board::Board;
 extern crate rand;
-use rand::thread_rng;
-use rand::Rng;
+use rand::prelude::*;
 
-pub fn random_move() {
-
+pub fn random_move(board: &mut Board) {
+    make_move(board);
 }
 
-fn make_move() {
-
+fn make_move(board: &mut Board) {
+    let _move: [[i8; 2]; 2] = choose_move(board);
+    board.move_piece(_move[0], _move[1]);
 }
 
-pub fn choose_move(board: &mut Board) -> [i8; 2] {
+fn choose_move(board: &mut Board) -> [[i8; 2]; 2] {
     // Opponent will always be black pieces
 
-    let mut moves: Vec<[i8; 2]> = Vec::new();
+    let mut piece_list: Vec<Box<dyn Moves>> = Vec::new();
     let mut piece: Box<dyn Moves>;
+    let mut moves: Vec<[i8; 2]>;
 
     for i in 0..8 {
         for j in 0..8 {
             if board.get_piece(i, j) < 0 {
                 piece = piece_type(board.get_piece(i, j), [i, j]);
-                let v: Vec<[i8; 2]> = piece.move_set(&board);
-                for m in 0..v.len() {
-                    moves.push(v[m]);
+                moves = piece.move_set(&board);
+
+                if moves.len() != 0 { //todo: Move out of loop
+                    piece_list.push(piece);
                 }
             }
         }
     }
-    let y: f64 = rng.gen_range(-10.0, 10.0);
-    println!("{:?}", moves[y]);
-    return moves[y];
+
+    let select = thread_rng().gen_range(0, piece_list.len());
+    moves = piece_list[select].move_set(&board);
+    let r = thread_rng().gen_range(0, moves.len());
+    
+    return [piece_list[select].get_pos(), moves[r]];
 }
