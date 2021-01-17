b3ce75a0af8c682f1816c6711f3c7aa4c93a0873
Sam Gebhardt <samgeb35@gmail.com>
2021-01-16 20:57:56 -0800

    Fixed syntax errors

--- src/AI/eval.rs
+++ src/AI/eval.rs
@@ -9,11 +9,11 @@ or negative modifier based on their current place on the board.
 Score = Material_value(White) – Material_value(Black) + Table_value(All the pieces on board).
 
 */
-use crate::Board:board;
+use crate::board::Board;
 
-fn pawn(pos: [i8; 2]) -> i32 {
+fn pawn(pos: [i8; 2]) -> i8 {
 
-    let PawnTable: [[i32; 8]; 8] = [
+    let PawnTable: [[i8; 8]; 8] = [
         [0,  0,  0,  0,  0,  0,  0,  0],
         [50, 50, 50, 50, 50, 50, 50, 50],
         [10, 10, 20, 30, 30, 20, 10, 10],
@@ -26,9 +26,9 @@ fn pawn(pos: [i8; 2]) -> i32 {
     return PawnTable[pos[0] as usize][pos[1] as usize];
 }
 
-fn tower(pos: [i8; 2]) -> i32 {
+fn tower(pos: [i8; 2]) -> i8 {
 
-    let TowerTable: [[i8; 2]; 8] = {
+    let TowerTable: [[i8; 8]; 8] = [
         [0,  0,  0,  0,  0,  0,  0,  0],
         [5, 10, 10, 10, 10, 10, 10,  5],
         [-5,  0,  0,  0,  0,  0,  0, -5],
@@ -36,15 +36,15 @@ fn tower(pos: [i8; 2]) -> i32 {
         [-5,  0,  0,  0,  0,  0,  0, -5],
         [-5,  0,  0,  0,  0,  0,  0, -5],
         [-5,  0,  0,  0,  0,  0,  0, -5],
-        [0,  0,  0,  5,  5,  0,  0,  0]]
-    }
+        [0,  0,  0,  5,  5,  0,  0,  0]];
+    
 
     return TowerTable[pos[0] as usize][pos[1] as usize];
 }
 
-fn knight(pos: [i8; 2]) -> i32 {
+fn knight(pos: [i8; 2]) -> i8 {
 
-    let KnightTable: [[i32; 8]; 8] = [
+    let KnightTable: [[i8; 8]; 8] = [
         [-50,-40,-30,-30,-30,-30,-40,-50],
         [-40,-20,  0,  0,  0,  0,-20,-40],
         [-30,  0, 10, 15, 15, 10,  0,-30],
@@ -57,9 +57,9 @@ fn knight(pos: [i8; 2]) -> i32 {
     return KnightTable[pos[0] as usize][pos[1] as usize];
 }
 
-fn bishop(pos: [i8; 2]) -> i32 {
+fn bishop(pos: [i8; 2]) -> i8 {
 
-    let BishopTable: [[i32; 8]; 8] = [
+    let BishopTable: [[i8; 8]; 8] = [
         [-20,-10,-10,-10,-10,-10,-10,-20],
         [-10,  0,  0,  0,  0,  0,  0,-10],
         [-10,  0,  5, 10, 10,  5,  0,-10],
@@ -72,7 +72,7 @@ fn bishop(pos: [i8; 2]) -> i32 {
     return BishopTable[pos[0] as usize][pos[1] as usize];
 }
 
-fn queen(pos: [i8; 2]) -> i32 {
+fn queen(pos: [i8; 2]) -> i8 {
 
     let QueenTable: [[i8; 8]; 8] = [
         [-20, -10, -10, -5, -5, -10, -10, -20],
@@ -82,15 +82,14 @@ fn queen(pos: [i8; 2]) -> i32 {
         [0, 0, 5, 5, 5, 5, 0, -5],
         [-10, 5, 5, 5, 5, 5, 0, -10],
         [-10, 0, 5, 0, 0, 0, 0, -10],
-        [-20, -10, -10, -5, -5, -10, -10, -20]]
-    ]
+        [-20, -10, -10, -5, -5, -10, -10, -20]];
 
     return QueenTable[pos[0] as usize][pos[1] as usize];
 }
 
-fn king(pos: [i8; 2]) -> i32 {
+fn king(pos: [i8; 2]) -> i8 {
 
-    let KingTable: [[i32; 8]; 8] = [
+    let KingTable: [[i8; 8]; 8] = [
         [-30, -40, -40, -50, -50, -40, -40, -30],
         [-30, -40, -40, -50, -50, -40, -40, -30],
         [-30, -40, -40, -50, -50, -40, -40, -30],
@@ -107,7 +106,7 @@ fn king(pos: [i8; 2]) -> i32 {
 // Both sides have no queens or
 // Every side which has a queen has additionally no other pieces or one minorpiece maximum.
 
-    // let KingTableEndGame: [[i32; 8]; 8] = [
+    // let KingTableEndGame: [[i8; 8]; 8] = [
     //     [-50,-40,-30,-20,-20,-30,-40,-50],
     //     [-30,-20,-10,  0,  0,-10,-20,-30],
     //     [-30,-10, 20, 30, 30, 20,-10,-30],
@@ -118,7 +117,7 @@ fn king(pos: [i8; 2]) -> i32 {
     //     [-50,-30,-30,-30,-30,-30,-30,-50]];
 
 
-fn find_piece(key: i8, pos: [i8; 2]) -> i32 {
+fn find_piece(key: i8, pos: [i8; 2]) -> i8 {
 
     // Queens/towers don't seem to have positioal advantage
     let p = match key {
@@ -145,9 +144,9 @@ fn base_value(key: i8) -> i32 {
         4 => 330,
         5 => 900,
         6 => 20000, 
-        _ => 
-    }
-
+        _ =>  100
+    };
+    return p;
 }
 
 
@@ -155,6 +154,6 @@ fn base_value(key: i8) -> i32 {
 // and position
 pub fn eval_board(board: &Board) {
 
-    find_piece(key, pos);
+    find_piece(1, [3, 2]);
 
 } 
